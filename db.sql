f238544bef5b8b8bd1199528b891a2a1cf967ae2
redouane <benbrahimredouane664@gmail.com>
2025-12-03 15:58:47 +0100

    feat: create table accounts

--- db.sql
+++ db.sql
@@ -2,3 +2,18 @@
 DROP DATABASE IF EXISTS bankly;
 CREATE DATABASE bankly;
 USE bankly;
+--create tables---------------------------------
+-- delete table if it exists
+DROP TABLE IF EXISTS `accounts`;
+--create table account
+CREATE TABLE `accounts` (
+    `account_id` int NOT NULL AUTO_INCREMENT,
+    `account_number` varchar(30) NOT NULL,
+    `balance` int DEFAULT NULL,
+    `account_type` enum('Checking', 'Savings', 'Business') DEFAULT NULL,
+    `customer_id` int DEFAULT NULL,
+    `advisor_id` int DEFAULT NULL,
+    PRIMARY KEY (`account_id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+    FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`)
+);
