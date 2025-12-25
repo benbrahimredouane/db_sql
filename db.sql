3b7b3f7d429e1e31b9d85739b97ab03708ad3bea
redouane <benbrahimredouane664@gmail.com>
2025-12-25 20:51:33 +0100

    chore(db.sql):delete unnecessary things

--- db.sql
+++ db.sql
@@ -1,27 +1,26 @@
 -- create tha database
-DROP DATABASE IF EXISTS bankly;
+
 CREATE DATABASE bankly;
 USE bankly;
 
 
 --create tables---------------------------------
--- delete table if it exists
-DROP TABLE IF EXISTS `accounts`;
+
 --create table account
-CREATE TABLE `accounts` (
-    `account_id` int NOT NULL AUTO_INCREMENT,
-    `account_number` varchar(30) NOT NULL,
-    `balance` int DEFAULT NULL,
-    `account_type` enum('Checking', 'Savings', 'Business') DEFAULT NULL,
-    `customer_id` int DEFAULT NULL,
-    `advisor_id` int DEFAULT NULL,
-    PRIMARY KEY (`account_id`),
-    FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
-    FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`)
+CREATE TABLE accounts (
+    account_id int  PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    account_number varchar(30) NOT NULL,
+    balance int DEFAULT NULL,
+    account_type enum('Checking', 'Savings', 'Business') DEFAULT NULL,
+    customer_id int DEFAULT NULL,
+    advisor_id int DEFAULT NULL,
+     
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+    FOREIGN KEY (advisor_id) REFERENCES advisors (advisor_id)
 );
 
 --@block insert data to table accounts
-INSERT INTO `accounts` 
+INSERT INTO accounts
 VALUES (1001,'ACC-1001',4000,'Checking',1,1),
 (1002,'ACC-1002',20000,'Savings',2,2),
 (1003,'ACC-1003',7500,'Business',3,3),
@@ -30,18 +29,17 @@ VALUES (1001,'ACC-1001',4000,'Checking',1,1),
 
 
 --@block
---delete table if iy exists
-DROP TABLE IF EXISTS `advisors`;
+
 
 --create table advisors
-CREATE TABLE `advisors` (
-  `advisor_id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(250) DEFAULT NULL,
-  `email` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`advisor_id`)
+CREATE TABLE advisors (
+  PRIMARY KEY advisor_id int NOT NULL AUTO_INCREMENT,
+  full_name varchar(250) DEFAULT NULL,
+  email varchar(250) DEFAULT NULL,
+  
 );
 -- fill data
-INSERT INTO `advisors`
+INSERT INTO advisors
 VALUES
  (1,'sara el9zdir','sarahel9zdir@gmail.com'),
  (2,'bochra el9rtit','el9rtit@gmail.com'),
@@ -51,19 +49,19 @@ VALUES
 
 --@block create table custoemrs 
 
-DROP TABLE IF EXISTS `customers`;
 
-CREATE TABLE `customers` (
-  `customer_id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(250) DEFAULT NULL,
-  `email` varchar(250) DEFAULT NULL,
-  `phone` varchar(15) DEFAULT NULL,
-  `registration_date` date DEFAULT NULL,
-  PRIMARY KEY (`customer_id`)
+
+CREATE TABLE customers (
+  PRIMARY KEY customer_id int NOT NULL AUTO_INCREMENT,
+  full_name varchar(250) DEFAULT NULL,
+  email varchar(250) DEFAULT NULL,
+  phone varchar(15) DEFAULT NULL,
+  registration_date date DEFAULT NULL,
+
 );
 
 --fill table customers
-INSERT into `customers`
+INSERT into customers
 VALUES
 (1,'ayman julal','aymane@example.com','0612345678','2025-01-01'),
 (2,'Jane Smith','jane@example.com','0698765432','2025-02-15'),
@@ -74,17 +72,14 @@ VALUES
 
 --@create table transactions
 
-DROP TABLE IF EXISTS `transactions`;
 
-CREATE TABLE `transactions` (
-  `transaction_id` int NOT NULL AUTO_INCREMENT,
-  `amount` int DEFAULT NULL,
-  `transaction_type` enum('debit','credit') DEFAULT NULL,
-  `transaction_date` date DEFAULT NULL,
-  `account_id` int DEFAULT NULL,
-  PRIMARY KEY (`transaction_id`),
-  KEY `account_id` (`account_id`),
-  FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
+CREATE TABLE transactions (
+  PRIMARY KEY transaction_id int NOT NULL AUTO_INCREMENT,
+  amount int DEFAULT NULL,
+  transaction_type enum('debit','credit') DEFAULT NULL,
+  transaction_date date DEFAULT NULL,
+  account_id int DEFAULT NULL,
+  FOREIGN KEY (account_id) REFERENCES accounts (account_id)
 );
 
 --@fill the transactions table
